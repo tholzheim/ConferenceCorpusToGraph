f3eb8f68c0c60f32e4826fc300b8e64efe19ad40
tholzheim <tim.holzheim@rwth-aachen.de>
2022-02-04 13:18:07 +0100

    Updated tests

--- tests/basetest.py
+++ tests/basetest.py
@@ -3,7 +3,10 @@ Created on 2021-08-19
 
 @author: wf
 '''
+import io
+from contextlib import redirect_stdout
 from os.path import expanduser
+from typing import Callable
 from unittest import TestCase
 import time
 import getpass
@@ -30,6 +33,7 @@ class Basetest(TestCase):
         msg=f"test {self._testMethodName}, debug={self.debug}"
         self.profiler=Profiler(msg,profile=self.profile)
         self.ensureWikiUserExist()
+        RawDataSources.downloadDblp(onlySample=True, forceUpdate=False)
         EventCorpus.download()
 
 
@@ -97,6 +101,23 @@ class Basetest(TestCase):
         wikiFileManager = WikiFileManager(wikiId, wikiTextPath, login=False, debug=debug)
         return wikiFileManager
 
+    @staticmethod
+    def captureOutput(fn: Callable, *args, **kwargs) -> str:
+        """
+        Captures stdout put of the given function
+
+        Args:
+            fn(callable): function to call
+        Returns:
+            str
+        """
+        f = io.StringIO()
+        with redirect_stdout(f):
+            fn(*args, **kwargs)
+        f.seek(0)
+        output = f.read()
+        return output
+
 class Profiler:
     '''
     simple profiler
@@ -124,3 +145,30 @@ class Profiler:
             print(f"{self.msg}{extraMsg} took {elapsed:5.1f} s")
         return elapsed
 
+class RawDataSources:
+    """
+    Manages the download of raw datasources that are needed to init the ConferenceCorpus
+    """
+
+    @classmethod
+    def downloadDblp(cls, onlySample:bool=True, forceUpdate:bool=False, debug=False):
+        """
+        download the dblp xml dump
+        Args:
+             onlySample(bool): If False the complete xml dump is downloaded (~4GB). Otherwise only a sample is downloaded.
+             forceUpdate(bool): If True the file will be downloaded even if already existent
+        """
+        sampleUrl = "https://github.com/WolfgangFahl/ConferenceCorpus/wiki/data/dblpsample.xml.gz"
+        dumpUrl = "https://dblp.uni-trier.de/xml/dblp.xml.gz"
+        if onlySample:
+            url=sampleUrl
+        else:
+            url = dumpUrl
+        home = expanduser("~")
+        output = Basetest.captureOutput(Download.downloadBackupFile, url=url,
+                                    fileName="dblp.xml",
+                                    targetDirectory=f"{home}/.dblp",
+                                    force=forceUpdate,
+                                    profile=True)
+        if debug:
+            print(output)
